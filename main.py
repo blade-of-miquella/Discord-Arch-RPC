dbff472efcfb61bc88f808c9d597bc840b6f37b9
blade-of-miquella <melinaprogg@gmail.com>
2026-01-25 17:46:45 +0300

    fix possible overflow error

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 import time
 import sys
-from pypresence import Presence
+from pypresence import Presence, DiscordNotFound
 
 CLIENT_ID = '1460668472647356499'
 
 def main() -> None:
-    try:
-        rpc = Presence(CLIENT_ID)
-        rpc.connect()
+    print("Starting Arch RPC...")
+    
+    while True:
+        try:
+            rpc = Presence(CLIENT_ID)
+            rpc.connect()
+            print("Connected to Discord!")
 
-        start_time = time.time()
+            start_time = time.time()
 
-        while True:
-            rpc.update(
-                details="using Arch btw",
-                large_image="arch_logo",
-                large_text="Arch Linux",
-                start=start_time
-            )
-            time.sleep(15)
-    except Exception as e:
-        time.sleep(20)
-        main()
+            while True:
+                rpc.update(
+                    details="using Arch btw",
+                    large_image="arch_logo",
+                    large_text="Arch Linux",
+                    start=start_time
+                )
+                time.sleep(15)
+                
+        except (DiscordNotFound, ConnectionResetError, Exception) as e:
+            time.sleep(20)
+            continue
 
 if __name__ == "__main__":
     main()
